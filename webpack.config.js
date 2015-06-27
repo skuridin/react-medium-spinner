c146bd1288b14baffbecbbf10ee060bec2fcf10f
Evgeniy Skuridin <redfield.ru@gmail.com>
2015-06-27 14:07:56 +0500

    Change dest dir

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   context: __dirname + '/src',
   entry: "./index.jsx",
   output: {
-    path: __dirname + '/dist',
+    path: __dirname,
     filename: "index.js",
     library: 'ReactMediumSpinner',
     libraryTarget: "umd"
