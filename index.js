b59163a39bafa3e8cf6770c84a9ff5e6a8772c4f
Evgeniy Skuridin <redfield.ru@gmail.com>
2015-07-12 18:16:03 +0500

    Build

--- index.js
+++ index.js
@@ -54,64 +54,91 @@ return /******/ (function(modules) { // webpackBootstrap
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	"use strict";
+	'use strict';
 
-	var React = __webpack_require__(1);
-
-	module.exports = React.createClass({
-	  displayName: "exports",
-
-	  getDefaultProps: function getDefaultProps() {
-	    return {
-	      active: false,
-	      speed: 20,
-	      color: "#60d778",
-	      height: "2px"
-	    };
-	  },
-	  getInitialState: function getInitialState() {
-	    return {
-	      width: 0,
-	      left: 0
-	    };
-	  },
-	  componentDidMount: function componentDidMount() {
-	    this.interval = setInterval(this.animate, this.props.speed);
-	  },
-	  componentWillUnmount: function componentWillUnmount() {
-	    clearInterval(this.interval);
-	  },
-	  animate: function animate() {
-	    if (this.props.active !== true) return false;
-
-	    var state = this.state;
-
-	    if (state.left === 0 && state.width < 100) {
-	      state.width += 4;
-	    } else if (state.width <= 100 && state.left < 100) {
-	      state.left += 4;
-	      state.width -= 4;
-	    } else if (state.left === 100 && state.width === 0) {
-	      state.left = 0;
-	    }
-
-	    this.setState(state);
-	  },
-	  render: function render() {
-	    var style = {
-	      display: this.props.active ? "block" : "none",
-	      position: "fixed",
-	      top: "0px",
-	      left: this.state.left + "%",
-	      right: "0px",
-	      width: this.state.width + "%",
-	      height: this.props.height,
-	      backgroundColor: this.props.color,
-	      zIndex: 800
-	    };
-	    return React.createElement("div", { style: style });
-	  }
+	Object.defineProperty(exports, '__esModule', {
+	  value: true
 	});
+
+	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+	var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) subClass.__proto__ = superClass; }
+
+	var _react = __webpack_require__(1);
+
+	var _react2 = _interopRequireDefault(_react);
+
+	var ReactMediumSpinner = (function (_React$Component) {
+	  function ReactMediumSpinner(props) {
+	    _classCallCheck(this, ReactMediumSpinner);
+
+	    _get(Object.getPrototypeOf(ReactMediumSpinner.prototype), 'constructor', this).call(this, props);
+	    this.state = { width: 0, left: 0 };
+	  }
+
+	  _inherits(ReactMediumSpinner, _React$Component);
+
+	  _createClass(ReactMediumSpinner, [{
+	    key: 'componentDidMount',
+	    value: function componentDidMount() {
+	      this.interval = setInterval(this.animate.bind(this), this.props.speed);
+	    }
+	  }, {
+	    key: 'componentWillUnmount',
+	    value: function componentWillUnmount() {
+	      clearInterval(this.interval);
+	    }
+	  }, {
+	    key: 'animate',
+	    value: function animate() {
+	      if (this.props.active !== true) return false;
+	      var state = this.state;
+	      if (state.left === 0 && state.width < 100) {
+	        state.width += 4;
+	      } else if (state.width <= 100 && state.left < 100) {
+	        state.left += 4;
+	        state.width -= 4;
+	      } else if (state.left === 100 && state.width === 0) {
+	        state.left = 0;
+	      }
+	      this.setState(state);
+	    }
+	  }, {
+	    key: 'render',
+	    value: function render() {
+	      var style = {
+	        display: this.props.active ? 'block' : 'none',
+	        position: 'fixed',
+	        top: '0px',
+	        left: this.state.left + '%',
+	        right: '0px',
+	        width: this.state.width + '%',
+	        height: this.props.height,
+	        backgroundColor: this.props.color,
+	        zIndex: 800
+	      };
+	      return _react2['default'].createElement('div', { style: style });
+	    }
+	  }]);
+
+	  return ReactMediumSpinner;
+	})(_react2['default'].Component);
+
+	exports['default'] = ReactMediumSpinner;
+
+	ReactMediumSpinner.defaultProps = {
+	  active: false,
+	  speed: 20,
+	  color: '#60d778',
+	  height: '2px'
+	};
+	module.exports = exports['default'];
 
 /***/ },
 /* 1 */
