20020df5435bfcf4014aa622da205f3a7aeb4d00
Evgeniy Skuridin <redfield.ru@gmail.com>
2015-09-13 12:32:03 +0500

    Refactor webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-var externals  = [],
-    entry      = "./dev.jsx",
-    output     = 'example.js',
-    HtmlPlugin = require('html-webpack-plugin');
+var externals = [];
+var entry = './dev.jsx';
+var output = 'example.js';
+var HTMLPlugin = require('html-webpack-plugin');
 
-if(process.env.NODE_ENV === "production") {
-  externals.push("react");
-  entry = "./index.jsx";
+if(process.env.NODE_ENV === 'production') {
+  externals.push('react');
+  entry = './index.jsx';
   output = 'index.js';
 }
 
@@ -16,21 +16,15 @@ module.exports = {
     path: __dirname,
     filename: output,
     library: 'ReactMediumSpinner',
-    libraryTarget: "umd"
+    libraryTarget: 'umd'
   },
   externals: externals,
-  module: {
-    loaders: [
-      { test: /\.jsx$/, loader: "babel" }
-    ]
-  },
+  module: { loaders: [{ test: /\.jsx$/, loader: 'babel' }] },
   plugins: [
-    new HtmlPlugin({
+    new HTMLPlugin({
       template: './src/template.html',
       hash: true,
-      minify: {
-        collapseWhitespace: true
-      }
+      minify: { collapseWhitespace: true }
     })
   ]
 };
