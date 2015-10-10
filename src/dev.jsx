671c06a2943db8d09b105464acad67f6d5080cef
Evgeniy Skuridin <redfield.ru@gmail.com>
2015-10-10 15:59:24 +0500

    Example using react 0.14

--- src/dev.jsx
+++ src/dev.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import MediumSpinner from './index.jsx';
 
-React.render(
+ReactDOM.render(
   <MediumSpinner active={true} />,
   document.getElementById('app')
 );
